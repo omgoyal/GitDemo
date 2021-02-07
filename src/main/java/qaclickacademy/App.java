d511d95d5a20723bedb7a94bda52b17633ebada9
omgoyal <osgoyal@gmail.com>
2021-02-07 20:17:39 +0530

    architech asian development

--- src/main/java/qaclickacademy/App.java
+++ src/main/java/qaclickacademy/App.java
@@ -15,5 +15,8 @@ public class App
         System.out.println( "Hello World!  3" );
         System.out.println( "Hello World!  4" );
         System.out.println( "Hello World!  5" );
+        
+        System.out.println( "Architect changes 1" );
+        System.out.println( "Architect changes 2" );
     }
 }
