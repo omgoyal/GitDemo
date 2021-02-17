001c3959f0483ed7587bb7a129a9db5349c27ef5
omgoyal <osgoyal@gmail.com>
2021-02-17 12:19:42 +0530

    numbersaddition class added

---
+++ src/main/java/qaclickacademy/NumbersAddition.java
@@ -0,0 +1,14 @@
+package qaclickacademy;
+
+public class NumbersAddition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println(" add two numbers in given table");
+		System.out.println("there are 100 numbers in list which is positve and negative as well");
+		
+	}
+
+}
