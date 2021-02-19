190ac39ba14d3bd760b45d89f10a683673f6fed6
omgoyal <osgoyal@gmail.com>
2021-02-19 12:12:53 +0530

    numbers multiply class added

---
+++ src/main/java/qaclickacademy/NumbersMultiply.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+public class NumbersMultiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("there are multiple numbers in table out of 1 to 100");
+      System.out.println("using table we can get expected output of numbers");
+	}
+
+}
