f145d97e69ca9fefcbf8bf9801e56470d27cf6f3
omgoyal <osgoyal@gmail.com>
2021-02-19 09:43:52 +0530

    number subtract file added

---
+++ src/main/java/qaclickacademy/NumbersSubtract.java
@@ -0,0 +1,12 @@
+package qaclickacademy;
+
+public class NumbersSubtract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("as per given multiple numbers we have to subtractony those numbers whose value is less than 100");
+		System.out.println("Subtraction result should be display in given format");
+	}
+
+}
