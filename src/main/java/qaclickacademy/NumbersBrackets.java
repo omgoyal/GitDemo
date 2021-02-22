d4e99ad30c49c6768b6858057254663108191706
omgoyal <osgoyal@gmail.com>
2021-02-22 14:31:17 +0530

    added numbers brackets file

---
+++ src/main/java/qaclickacademy/NumbersBrackets.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+public class NumbersBrackets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("there are there type of brackets small, medium and large brackets in equation");
+        System.out.println("As per order user need to solve first small then medium the large brackets given any equation");
+	}
+
+}
