8e78b5809c5fc0f04ad706b3a320f595d094fd1c
omgoyal <osgoyal@gmail.com>
2021-02-22 15:33:29 +0530

    remove medium the large brackets

--- src/main/java/qaclickacademy/NumbersBrackets.java
+++ src/main/java/qaclickacademy/NumbersBrackets.java
@@ -5,7 +5,7 @@ public class NumbersBrackets {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("there are there type of  large brackets in equation");
-        System.out.println("As  to solve first small then medium the large ");
+        System.out.println("As  to solve first small then ");
 	}
 
 }
