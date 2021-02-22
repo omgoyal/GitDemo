c198e62fd206d1fd64e184c7784d6d3599f208c7
omgoyal <osgoyal@gmail.com>
2021-02-22 14:13:49 +0530

    added numbersdivide file

---
+++ src/main/java/qaclickacademy/NumbersDivide.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+public class NumbersDivide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("There are BOMMAs rule has been applied while division of multiple numbers where addition, subtraction, multiplication with brackets are given");
+	System.out.println("Result should be accurate appliying all rules as per given sheet ");
+	}
+
+}
