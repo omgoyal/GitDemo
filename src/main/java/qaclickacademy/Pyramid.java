680a7fec50d7cf2905f4330ce1bc8fd9e89b0343
omgoyal <osgoyal@gmail.com>
2021-02-26 10:27:33 +0530

    Added pyramid class file

---
+++ src/main/java/qaclickacademy/Pyramid.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("pyramid is one of seven wonders in world ");
+ System.out.println("Pyramid constrct on the basis of stars which has been knowns as giza statue of liberty ");
+	}
+
+}
