4f4c011e42cb4ec46554808744e73ebd58f6058e
omgoyal <osgoyal@gmail.com>
2021-03-08 11:20:03 +0530

    Triangle file updated

--- src/main/java/qaclickacademy/Triangle.java
+++ src/main/java/qaclickacademy/Triangle.java
@@ -4,7 +4,8 @@ public class Triangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+  System.out.println("Traingle is one of the shape of mathmatics it has three sides of line which creates a triangle..");
+  System.out.println("Square is one the shape in mathmatics it has four side of line which creates a ");
 	}
 
 }
