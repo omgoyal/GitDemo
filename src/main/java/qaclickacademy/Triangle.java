1de68c192b991f882b9cae0637871fa2ddb9df6d
omgoyal <osgoyal@gmail.com>
2021-03-09 12:24:49 +0530

    Added keyword again

--- src/main/java/qaclickacademy/Triangle.java
+++ src/main/java/qaclickacademy/Triangle.java
@@ -5,7 +5,7 @@ public class Triangle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("Traingle is one of the shape of mathmatics it has three sides of line which creates a triangle..");
-  System.out.println("Square is one the shape in mathmatics it  of line which creates a ");
+  System.out.println("Square is one the shape in mathmatics it has four side of line which creates a ");
 	}
 
 }
