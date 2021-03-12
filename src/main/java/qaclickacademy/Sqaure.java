2b18af6165f17d987b4f48ddc99d346fb300f5eb
omgoyal <osgoyal@gmail.com>
2021-03-12 09:59:18 +0530

    Added sqaure file

---
+++ src/main/java/qaclickacademy/Sqaure.java
@@ -0,0 +1,10 @@
+package qaclickacademy;
+
+public class Sqaure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Square has four sides in one of the shape in mathmatics. it has tow diagonal which has been cross each other at 90 degree.");
+	}
+
+}
