1ca4f12ff43576d61770640861a4e581452db20b
omgoyal <osgoyal@gmail.com>
2021-05-07 09:18:40 +0530

    Hexagon file added

---
+++ src/main/java/qaclickacademy/Hexagon.java
@@ -0,0 +1,10 @@
+package qaclickacademy;
+
+public class Hexagon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("There are six side of any hexagon shape. Each side create an 120 degree angle with another side");
+	}
+
+}
