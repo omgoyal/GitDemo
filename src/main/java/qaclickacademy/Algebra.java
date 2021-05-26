282b643de594e9194712c37aad18e890421c5094
omgoyal <osgoyal@gmail.com>
2021-05-26 09:36:56 +0530

    Algebra file added

---
+++ src/main/java/qaclickacademy/Algebra.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+public class Algebra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("In algebra there are quadraticm equation whichs is very useful for solvings numbers value");
+	}
+
+}
