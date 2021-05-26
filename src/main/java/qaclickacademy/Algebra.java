847da36873db8857e3ef97255669c876853390a6
omgoyal <osgoyal@gmail.com>
2021-05-26 11:07:29 +0530

    Changes in algebra file

--- src/main/java/qaclickacademy/Algebra.java
+++ src/main/java/qaclickacademy/Algebra.java
@@ -5,7 +5,7 @@ public class Algebra {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("In algebra there are quadraticm equation whichs is very useful for solvings numbers value");
+		System.out.println("In algebra there are quadraticm equation whichs is very useful ");
 	}
 
 }
