6414a549a160275caf5436c558c617c4812b5d51
omgoyal <osgoyal@gmail.com>
2021-06-18 09:37:58 +0530

    updated calculus file

--- src/main/java/qaclickacademy/Calculus.java
+++ src/main/java/qaclickacademy/Calculus.java
@@ -4,7 +4,7 @@ public class Calculus {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("Calculus is one of the subject in mathmatics where we can learn differtiatiens and integratiens ");
+     System.out.println("Calculus is one of the subject in  we can learn differtiatiens and integratiens ");
 	}
 
 }
